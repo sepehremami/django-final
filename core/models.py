5e1e11fe30cfb66992bd0d6da57277046861ad49
sepehremami <sepehr7890@gmail.com>
2023-05-13 22:51:13 -0400

    add basemodel, update settings

--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class BaseModel(models.Model):
+    '''
+    Common attributes of all modesl
+    All models extend BaseModel
+    '''
+    is_deleted = models.BooleanField(default=False)
+    create_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+
+
+class User(AbstractUser):
+    pass
+
+
+class Visitor(User):
+    is_visitor = True
+
+    class Meta: 
+        proxy = True
