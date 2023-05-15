6cf8cbdf0cff6d8843f08ebb89a2a83f6fd2ccf0
sepehremami <sepehr7890@gmail.com>
2023-05-15 10:27:41 -0400

    moved shop to apps dir

--- shop/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Product, Cart, CartItem, Category, ShoppingSession
-
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Category)
-admin.site.register(ShoppingSession)
--- shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", view=views.ProductListView.as_view(), name="products"),
-    path("<int:pk>", view=views.ProductDetailView.as_view(), name="product"),
-]
