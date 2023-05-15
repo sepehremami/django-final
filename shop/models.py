384d559f75cbfd3fc5c45537703fa97090be9ceb
sepehremami <sepehr7890@gmail.com>
2023-05-15 03:31:39 -0400

    add shopping cart model

--- shop/models.py
+++ shop/models.py
@@ -1,22 +1,36 @@
 from django.db import models
 from core.models import BaseModel, User
 
+
 class ShoppingSession(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    total = models.DecimalField(null=True, blank=True)
+
 
 class Cart(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    session_id = models.CharField(max_length=500)
-    status = models.smalint
+    session_id = models.(max_length=500)
+    status = models.CharField(
+        max_length=3,
+        choices=[
+            ("A", "Available"),
+            ("U", "Unavailable"),
+        ],
+        default="U",
+    )
+    total = models.DecimalField(null=True, blank=True)
+
+    
+
 
 
 class Product(BaseModel):
     name = models.CharField(max_length=150)
     desc = models.TextField()
     sku = models.CharField(max_length=100)
-    category = models.ForeignKey('Category', on_delete=models.RESTRICT, null=True, blank=True)
+    category = models.ForeignKey(
+        "Category", on_delete=models.RESTRICT, null=True, blank=True
+    )
     price = models.DecimalField()
-    discount = models.ForeignKey('Discount', on_delete=models.RESTRICT, null=True, blank=True)
-
-
+    discount = models.ForeignKey(
+        "Discount", on_delete=models.RESTRICT, null=True, blank=True
+    )
