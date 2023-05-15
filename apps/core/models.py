6cbb664457f447aa1a0e576208fbe4a0720e0c1b
sepehremami <sepehr7890@gmail.com>
2023-05-15 10:29:13 -0400

    create apps dir, organize projects;

---
+++ apps/core/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django_jalali.db import models as jmodels
+
+
+class BaseModel(models.Model):
+    """
+    Common attributes of all modesl
+    All models extend BaseModel
+    """
+
+    is_deleted = models.BooleanField(default=False, editable=False )
+    create_date = jmodels.jDateField(auto_now_add=True)
+    modified_date = jmodels.jDateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+
+class User(AbstractUser):
+    pass
+
+
+class Visitor(User):
+    is_visitor = True
+
+    class Meta:
+        proxy = True
---
+++ apps/shop/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", view=views.ProductListView.as_view(), name="products"),
+    path("<int:pk>", view=views.ProductDetailView.as_view(), name="product"),
+]
