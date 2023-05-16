05185afdc8e413edafbe4e0707d2579c91a2422d
sepehremami <sepehr7890@gmail.com>
2023-05-16 06:23:06 -0400

    jalali datetime

--- config/settings.py
+++ config/settings.py
@@ -109,19 +109,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "fa-ir"
+import locale
 
-TIME_ZONE = "UTC"
+locale.setlocale(locale.LC_ALL, "fa_IR.UTF-8")
+
+TIME_ZONE = "Asia/Tehran"
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
