9b56d4c1ee84efdf3f6ac4560cc45a651fa8c681
sepehremami <sepehr7890@gmail.com>
2023-05-17 16:18:11 -0400

    write wishlist login requierd

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", view=views.ProductListView.as_view(), name="products"),
     path("<int:pk>", view=views.ProductDetailView.as_view(), name="product"),
+    path("wishlist", view=views.wishlist, name="wish-list"),
+
 ]
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View, CreateView, ListView, DetailView
 from .models import Product
@@ -13,3 +15,8 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = "shop/product_detail.html"
     context_object_name = "product"
+
+
+@login_required(login_url='/accounts/login')
+def wishlist(request):
+    return HttpResponse('added')
