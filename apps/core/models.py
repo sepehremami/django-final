3ea2ff09336fc168809adb5338102ffde3f1d1d1
sepehremami <sepehr7890@gmail.com>
2023-05-17 18:55:05 -0400

    create command that creates customer and permissions

--- apps/core/models.py
+++ apps/core/models.py
@@ -33,3 +33,26 @@ class Visitor(User):
 
     class Meta:
         proxy = True
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+
+class Command(BaseCommand):
+    help = 'Creates the "customer" group with the desired permissions'
+
+    def handle(self, *args, **options):
+        # Create the "customer" group
+        customer_group, created = Group.objects.get_or_create(name='customer')
+
+        # Add the "add_wishlist" permission to the "customer" group
+        add_wishlist_permission = Permission.objects.get(codename='add_wishlist')
+        customer_group.permissions.add(add_wishlist_permission)
+
+        # Remove the "delete_wishlist" permission from the "customer" group
+        delete_wishlist_permission = Permission.objects.get(codename='delete_wishlist')
+        customer_group.permissions.remove(delete_wishlist_permission)
+
+        # Save the changes to the "customer" group
+        customer_group.save()
+
+        self.stdout.write(self.style.SUCCESS('Successfully created the "customer" group with the desired permissions.'))
