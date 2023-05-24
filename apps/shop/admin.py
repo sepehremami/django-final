a669a7b4914742b24fdf8f9e271218374d00c0dd
sepehremami <sepehr7890@gmail.com>
2023-05-24 15:35:00 -0400

    add admin inlines

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -4,15 +4,38 @@ from django_jalali.admin.filters import JDateFieldListFilter
 import django_jalali.admin as jadmin
 
 
+class SubProductInline(admin.TabularInline):
+    model = SubProduct
+
+
+class SubCategoryInline(admin.TabularInline):
+    model = Category
+    fields = ['name']
+
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_filter = (("create_at", JDateFieldListFilter),)
+    list_filter = (("create_date", JDateFieldListFilter),)
+    fieldsets = (
+        ("general", {"fields": ("name", 'category')}),
+        ("other", {"fields": ("desc", 'image')}),
+    )
+    inlines = (SubProductInline,)
 
 
-admin.site.register(Pricing)
+@admin.register(Pricing)
+class PricingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (SubCategoryInline,)
+
+
 admin.site.register(ProductColour)
 admin.site.register(SubProduct)
-admin.site.register(Product)
+
 admin.site.register(CartItem)
-admin.site.register(Category)
 admin.site.register(Media)
 admin.site.register(ProductReview)
