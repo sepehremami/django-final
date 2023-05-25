80959706696201b018a32f5fc2d263dfa1f4e562
sepehremami <sepehr7890@gmail.com>
2023-05-24 20:32:42 -0400

    complete coursel

--- apps/core/views.py
+++ apps/core/views.py
@@ -3,7 +3,7 @@ from apps.shop.models import Product, Category
 
 
 def home(request):
-    category = Category.objects.all()[:6]
+    category = Category.objects.all()
     products = Product.objects.all()[:6]
     return render(request, 'landing.html', {'products': products, 'category':category})
 
