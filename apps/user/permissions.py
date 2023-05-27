4e5884d75e635f4da7d2e3ecf9341f8d33ba25f8
Sepehr <sepehr7890@gmail.com>
2023-05-27 07:29:58 +0430

    handling permissions

--- apps/user/permissions.py
+++ apps/user/permissions.py
@@ -4,19 +4,27 @@ from django.contrib.contenttypes.models import ContentType
 
 staff_group, staff_created = Group.objects.get_or_create(name='Staff')
 manager_group, manager_created = Group.objects.get_or_create(name='Manager')
+spectator, spectator_created = Group.objects.get_or_create(name='Spectator')
 if manager_created:
-    # Add any relevant custom permission classes.
     add_product_permission = Permission.objects.get(codename='add_product')
     manager_group.permissions.add(add_product_permission)
 
-    # Get the content type object for the Fish model.
-    fish_content_type = ContentType.objects.get(app_label='shop', model='Product')
-    # Get all available permissions for this content type.
-    all_fish_permissions = Permission.objects.filter(content_type=fish_content_type)
-    # Retrieve the Manager group from above.
+    product_content_type = ContentType.objects.get(app_label='shop', model='Product')
+    all_product_permissions = Permission.objects.filter(content_type=product_content_type)
     manager_group = Group.objects.get(name='Manager')
-    # Assign all fish-related permissions to manager group.
-    for permission in all_fish_permissions:
+    for permission in all_product_permissions:
+        manager_group.permissions.add(permission)
+
+    user_content_type = ContentType.objects.get(app_label='shop', model='user')
+    all_user_permissions = Permission.objects.filter(content_type=user_content_type)
+    manager_group = Group.objects.get(name='Manager')
+    for permission in all_user_permissions:
+        manager_group.permissions.add(permission)
+
+    category_content_type = ContentType.objects.get(app_label='shop', model='category')
+    all_category_permissions = Permission.objects.filter(content_type=category_content_type)
+    manager_group = Group.objects.get(name='Manager')
+    for permission in all_category_permissions:
         manager_group.permissions.add(permission)
 
 
@@ -24,3 +32,14 @@ if staff_created:
     add_product_permission = Permission.objects.get(codename='add_product')
 
     manager_group.permissions.add(add_product_permission)
+    cart_content_type = ContentType.objects.get(app_label='shop', model='cart')
+    all_cart_permissions = Permission.objects.filter(content_type=cart_content_type)
+    manager_group = Group.objects.get(name='Manager')
+    for permission in all_cart_permissions:
+        manager_group.permissions.add(permission)
+
+if spectator_created:
+     add_product_permission = Permission.objects.get(codename='view_product')
+     add_category_permission = Permission.objects.get(codename='view_category')
+     add_user_permission = Permission.objects.get(codename='view_user')
+     add__permission = Permission.objects.get(codename='view_all')
