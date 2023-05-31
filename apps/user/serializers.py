b28de8f6c37b463949ac9ef8d6d6e1eb78b53839
Sepehr <sepehr7890@gmail.com>
2023-06-01 00:49:34 +0430

    address serializer

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -1,6 +1,9 @@
+import re
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from apps.user.models import User
+from rest_framework import viewsets
+from .models import Address
 
 
 class ObtainTokenSerializer(serializers.Serializer):
@@ -12,3 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     fields = ["id", "first_name", "last_name", "username"]
+
+
+class AddressSerializer(serializers.ModelSerializer):
+  class Meta: 
+    model = Address
+    fields = ['user','receiver','province','city','addr','zip_code','phone','is_default']
+
+  def validate_zipcode(self, value):
+    pattern = r'^\d{5}$'
+    
+    if not bool(re.match(pattern, value)):
+      raise serializers.ValidationError('Invalid zip code format')
+
+    
+
+
+
