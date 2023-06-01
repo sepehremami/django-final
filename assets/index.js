870b62a382254138dc5e5de4a045b3da0fec6276
Sepehr <sepehr7890@gmail.com>
2023-06-01 04:40:18 +0430

    add djangoClient class

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,50 @@
 import config from './config.js';
+import Cookies from 'js-cookie';
 
 
+class DjangoClient {
+  constructor(overrides) {
+    this.config = {
+      ...config,
+      ...overrides,
+    };
+    this.apiClient = this.getApiClient(this.config);
+  }
 
-console.log(config.apiURL)
+  getApiClient(config) {
+    const initialConfig = {
+      baseURL: `${config.apiURL}`,
+    };
+    const client = axios.create(initialConfig);
+    client.interceptors.request.use(CookieInterceptor);
+    return client;
+  }
 
-axios.get(config.apiURL)
-    .then((response)=> {
-        console.log(response)
+  getClientAddresses = () => {
+    return new Promise((resolve, reject) => {
+      this.apiClient.get('/user/api/address/')
+        .then(response => {
+          resolve(response.data)
+        })
+        .catch((error) => {
+          console.log(error);
+          reject(error)
+        })
     })
-    .catch((error)=> {
-        console.log(error)
-    })
+  };
+}
+
+function CookieInterceptor(config) {
+  const headers = {};
+  const authToken = Cookies.get('access');
+  if (authToken) {
+    headers['Authorization'] = `Bearer ${authToken}`;
+  }
+  else {
+    alert('Session expired! Please login again!');
+  }
+  config['headers'] = headers;
+  return config;
+};
+
+export default DjangoClient;
