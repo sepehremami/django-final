3a4dab2647d440d22a60286f541a769dae140da3
Sepehr <sepehr7890@gmail.com>
2023-06-03 08:01:18 +0430

    order views and urls

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r"order", views.OrderInfoViewSet, basename="order")
+router.register(r"order-item", views.OrderItemViewSet, basename="order-item")
+router.register(r'product-discount', views.ProductDiscountViewSet, basename="product-discount")
+router.register(r'category-discount', views.CategoryDiscountViewSet, basename="category-discount")
+
+
 urlpatterns = [
     path('<int:product_id>', views.add_to_cart, name='add-to-cart'),
     path('home', views.add_to_cart, name='home'),
+    path("api/", include(router.urls)),
+
 ]
