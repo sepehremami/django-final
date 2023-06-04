1f7233132030c000717492d0eee0d4843ae471ac
Sepehr <sepehr7890@gmail.com>
2023-06-04 11:02:01 +0430

    add user admin

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+
 # from .permissions import *
 
-admin.site.register(User)
 admin.site.register(Membership)
 admin.site.register(UserRewardLog)
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
-
     list_display=['user','receiver','province','city','addr','zip_code','phone','is_default',]
+
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_superuser', 'last_login')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    actions = ['make_staff',]
+
+    def make_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+
+    
+    
+
+admin.site.register(User, UserAdmin)
