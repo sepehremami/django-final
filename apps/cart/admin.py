325c8384b54cbc060c0c1a97980037f4a0459df2
Sepehr <sepehr7890@gmail.com>
2023-06-04 10:59:13 +0430

    refactor cart admin

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import *
 
+@admin.register(CategoryDiscount)
+class CategoryDiscountAdmin(admin.ModelAdmin):
+    list_display = ['discount_value', 'discount_unit','valid_until','expired','minimum_order_value','maximum_discount_amount','category']
 
-admin.site.register(OrderInfo)
-admin.site.register(OrderItem)
-admin.site.register(CategoryDiscount)
-admin.site.register(ProductDiscount)
 
+@admin.register(ProductDiscount)
+class ProductDiscountAdmin(admin.ModelAdmin):
+    list_display = ['discount_value', 'discount_unit','valid_until','expired','minimum_order_value','maximum_discount_amount','product']
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'count']
+
+
+@admin.register(OrderInfo)
+class OrderInfoAdmin(admin.ModelAdmin):
+    list_display = ['order_id' ,'user' ,'addr' ,'total_count' ,'total_price' ,'order_status']
