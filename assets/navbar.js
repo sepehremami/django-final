5305f01d6d183fcbda04de88d645c3c52e0db0a6
Sepehr <sepehr7890@gmail.com>
2023-06-12 12:18:44 +0430

    shopping cart cookie expire

--- assets/navbar.js
+++ assets/navbar.js
@@ -4,27 +4,29 @@ import config from "./config";
 
 
 const login = document.getElementById('login');
+const shoppingCart = document.getElementById('shopping-cart');
+
 const routes = {
     "loggedIn": "/user/profile/",
-    "notLoggedIn": "/user/login/"
+    "notLoggedIn": "/accounts/login/"
 }
+
 
 const authCookie = Cookies.get('access');
 if (authCookie) {
     const decoded = jwt_decode(authCookie)
-    const expire = (decoded.exp * 1000 ) -Date.now();
+    const expire = (decoded.exp * 1000 ) - Date.now();
     console.log(expire)
 
     if (decoded.exp < Date.now() / 1000) {
         Cookies.remove('access'); // Delete expired token immediately
-        window.location.href = config.apiURL + notLoggedIn;
         
     } else  {
         setTimeout( ()=> {
             Cookies.remove('access')
-            window.location.href = config.apiURL + notLoggedIn
-        }, 5000 )
+        }, 500000 )
         login.href = config.apiURL + routes.loggedIn
+        console.log
     }
 
 } else {
