5793bd2b8cbd8b477cb44fed66f2929d4608c6b1
Sepehr <sepehr7890@gmail.com>
2023-06-15 00:57:13 +0430

    with last

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from apps.cart.models import OrderInfo
+from apps.cart.models import OrderInfo, CartDiscount
 from apps.user.backends import JWTAuthBackend
 from apps.user.models import User, Address
 from django.contrib.auth.views import TemplateView
@@ -8,51 +8,67 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from apps.user.tasks import send_otp_code
-from .serializers import ObtainTokenSerializer, UserRegisterSerializer, UserSerializer, AddressSerializer, OTPCodeSerializer
+from .serializers import (
+    AddressPostSerializer,
+    ObtainTokenSerializer,
+    UserRegisterSerializer,
+    UserSerializer,
+    AddressSerializer,
+    OTPCodeSerializer,
+    ChangePasswordSerializer,
+)
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework import permissions, views as api_views
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import authenticate, login
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.authtoken.views import ObtainAuthToken
 from django.core.cache import cache
 from rest_framework import status
 from django.shortcuts import redirect
 from django.urls import reverse
+from drf_yasg.renderers import SwaggerUIRenderer
 
 
 User = get_user_model()
 
 from django.contrib.auth.middleware import AuthenticationMiddleware
 
+
 class AuthenticationRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
-        auth_cookie = request.COOKIES.get('access')
+        auth_cookie = request.COOKIES.get("access")
         if auth_cookie is None:
-            return redirect(reverse('landing'))
+            return redirect(reverse("landing"))
         return super().dispatch(request, *args, **kwargs)
-        
 
+
+from apps.user.forms import UpdateUserForm
+from jwt import decode
 
 class ProfileView(AuthenticationRequiredMixin, TemplateView):
     model = User
     template_name = "user/profile.html"
 
+    def get(self, *args, **kwargs):
+        id = kwargs.get("pk")
+        user = User.objects.get(pk=id)
+        user_update_form = UpdateUserForm(instance=user)
+        context = self.get_context_data()
+        context["user_form"] = user_update_form
+        return render(self.request, "user/profile.html", context)
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data()
         user = self.request.user
         if isinstance(user, User):
             addresses = Address.objects.filter(user=user)
-            context['addresses'] = addresses
+            context["addresses"] = addresses
             orders = OrderInfo.objects.filter(user=user)
-            context['orders'] =orders      
+            context["orders"] = orders
         return context
-
-
 
 
 # TODO: write otp authentication back
@@ -61,7 +77,9 @@ class ObtainTokenView(ObtainAuthToken):
     serializer_class = ObtainTokenSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer: ObtainTokenSerializer = self.serializer_class(data=request.data,  context={'request': request})
+        serializer: ObtainTokenSerializer = self.serializer_class(
+            data=request.data, context={"request": request}
+        )
         serializer.is_valid(raise_exception=True)
         # alg_otp_code = serializer.validated_data.get('otp_code')
         username_or_phone = serializer.validated_data.get("username")
@@ -72,11 +90,9 @@ class ObtainTokenView(ObtainAuthToken):
         print(user)
         print(user.check_password(password))
         if user is None or not user.check_password(password):
-            return Response({"message": "Invalid Credentials"}, status=status.HTTP_403_FORBIDDEN)
-
-        # real_otp_code = cache.get(user.phone_number)
-        # if alg_otp_code != real_otp_code:
-        #     return Response({'Error': 'otp code not right!'}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"message": "Invalid Credentials"}, status=status.HTTP_403_FORBIDDEN
+            )
 
         jwt_token = JWTAuthBackend.create_jwt(user)
 
@@ -91,23 +107,25 @@ class ProfileViewSet(viewsets.ModelViewSet):
         """
         Return a queryset of the user itself for the authenticated self.
         """
-        print(self.kwargs.get('pk'))
+        print(self.kwargs.get("pk"))
         if isinstance(self.request.user, User):
-            return User.objects.filter(id=self.kwargs.get('pk'))
+            return User.objects.filter(id=self.kwargs.get("pk"))
 
 
 class AddressViewSet(viewsets.ModelViewSet):
     serializer_class = AddressSerializer
     permission_classes = [permissions.IsAuthenticated]
-    
+
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
 
         data = request.data.copy()
-        data['is_default'] = True
+        data["is_default"] = True
 
         user = request.user
-        Address.objects.filter(user=user).exclude(id=instance.id).update(is_default=False)
+        Address.objects.filter(user=user).exclude(id=instance.id).update(
+            is_default=False
+        )
 
         serializer = self.get_serializer(instance, data=data, partial=True)
         serializer.is_valid(raise_exception=True)
@@ -119,10 +137,14 @@ class AddressViewSet(viewsets.ModelViewSet):
         """
         Return a queryset of all addresses for the authenticated user.
         """
-        if isinstance(self.request.user,User):
-            return Address.objects.filter(user=self.request.user)
+        if isinstance(self.request.user, User):
+            return Address.objects.filter(user=self.request.user, is_deleted=False)
 
 
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == 'create':
+            return AddressPostSerializer
+        return AddressSerializer
 
 
 @api_view(["GET"])
@@ -133,46 +155,62 @@ def index(request):
     return Response("failed!")
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 @csrf_exempt
 @permission_classes([permissions.AllowAny])
 def send_otp(request):
     serializer = OTPCodeSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
-    username = serializer.validated_data.get('username')
-    password = serializer.validated_data.get('password')
+    phone = serializer.validated_data.get("phone")
+    send_otp_code.delay(phone)
+    return Response({"success": "OTP code sent!"}, status=200)
 
-    user = User.objects.get(username=username)
-    if user is None or not user.check_password(password):
-            return Response({"message": "Invalid Credentials"}, status=status.HTTP_403_FORBIDDEN)
 
-    if not user.phone_number:
-        return Response({'error': 'Field required'}, status=status.HTTP_400_BAD_REQUEST)
-    
-    send_otp_code.delay(user.phone_number)
-    
+from .serializers import OTPValidCodeSerializer
 
-    return Response({'success':'OTP code sent!'}, status=200)
+
+@api_view(["POST"])
+@permission_classes([permissions.AllowAny])
+def validate_opt(request):
+    serializer = OTPValidCodeSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    otp = serializer.validated_data.get("otp_code")
+    otp_code = request.data.get("otp_code")
+    phone = request.data.get("phone")
+    real_otp_code = cache.get(phone)
+
+    if otp_code != real_otp_code:
+        print("h")
+        return Response(
+            {"Error": "otp code not right!"}, status=status.HTTP_403_FORBIDDEN
+        )
+
+    user = User.objects.get(phone_number=phone)
+    if user is None:
+        print("k")
+        return Response(
+            {"message": "Invalid Credentials"}, status=status.HTTP_403_FORBIDDEN
+        )
+
+    jwt_token = JWTAuthBackend.create_jwt(user)
+    return Response({"token": jwt_token})
 
 
 @api_view()
 @permission_classes([permissions.IsAuthenticated])
 def my_login(request):
     user = request.user
-    
+
     if user:
-        # Set User ID into session.
         request.session["member_id"] = user.id
-        
-        # Log in the authenticated user using Django's built-in login() method.
-        django_request = request._request  # Get underlying HttpRequest from DRF Request instance.
+
+        django_request = request._request
         login(django_request, user)
-        
+
         return Response(" You're logged in.")
-    
+
     else:
         return Response("Your username and password didn't match.")
-
 
 
 from rest_framework import viewsets, status
@@ -181,14 +219,85 @@ from .serializers import UserSerializer
 from rest_framework.generics import CreateAPIView
 
 
+
 class RegisterViewSet(CreateAPIView):
     model = User
     serializer_class = UserRegisterSerializer
-
 
     def create(self, request):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            return Response({'success': True, 'user_id': user.id}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"success": True, "user_id": user.id}, status=status.HTTP_201_CREATED
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+from rest_framework.generics import UpdateAPIView
+
+
+class ChangePasswordView(UpdateAPIView):
+    """
+    An endpoint for changing password.
+    """
+
+    serializer_class = ChangePasswordSerializer
+    model = User
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self, queryset=None):
+        obj = self.request.user
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            if not self.object.check_password(serializer.data.get("old_password")):
+                return Response(
+                    {"old_password": ["Wrong password."]},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            self.object.set_password(serializer.data.get("new_password"))
+            self.object.save()
+            response = {
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Password updated successfully",
+                "data": [],
+            }
+
+            return Response(response)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CartTemplateView(AuthenticationRequiredMixin, TemplateView):
+    model = get_user_model()
+    template_name = 'cart/cart.html'
+
+    def get_context_data(self, *args, **kwargs):
+        user = kwargs.get('pk')
+        print('pk', user)
+        orders = OrderInfo.objects.get(user_id=user, order_status=2)
+        print('orders', orders)
+        context = super().get_context_data()
+        context['cart'] = orders
+        return context
+
+
+class FinalOrderTemplateView(AuthenticationRequiredMixin, TemplateView):
+    model = get_user_model()
+    template_name = 'cart/order.html'
+
+    def get_context_data(self, *args, **kwargs):
+        user = kwargs.get('pk')
+        print('pk', user)
+        orders = OrderInfo.objects.get(user_id=user, order_status=2)
+        print('orders', orders)
+        context = super().get_context_data()
+        context['cart'] = orders
+        return context
+
