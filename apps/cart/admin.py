cc7b19c52845626254133abcb80010176bf69189
Sepehr <sepehr7890@gmail.com>
2023-06-15 00:52:36 +0430

    cart discount admin

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -3,8 +3,11 @@ from .models import *
 
 @admin.register(CategoryDiscount)
 class CategoryDiscountAdmin(admin.ModelAdmin):
-    list_display = ['discount_value', 'discount_unit','valid_until','expired','minimum_order_value','maximum_discount_amount','category']
+    list_display = ['id', 'discount_value', 'discount_unit','valid_until','expired','minimum_order_value','maximum_discount_amount','category']
 
+@admin.register(CartDiscount)
+class CartDiscountAmin(admin.ModelAdmin):
+    list_display = ('id', 'code','discount_value', 'discount_unit','valid_until','expired','minimum_order_value','maximum_discount_amount','order')
 
 @admin.register(ProductDiscount)
 class ProductDiscountAdmin(admin.ModelAdmin):
@@ -18,4 +21,4 @@ class OrderItemAdmin(admin.ModelAdmin):
 
 @admin.register(OrderInfo)
 class OrderInfoAdmin(admin.ModelAdmin):
-    list_display = ['order_id' ,'user' ,'addr' ,'total_count' ,'total_price' ,'order_status']
+    list_display = ['id', 'order_id' ,'user' ,'addr' ,'total_count' ,'total_price' ,'order_status']
